16b1743bd11be345cd021a10a345efed3ae2dfd3
damyz08 <damyz08solano@gmail.com>
2021-09-27 11:57:13 -0500

    Tarea realizada

---
+++ README.md
@@ -0,0 +1,1 @@
+Actividad de la calculadora con javascript, desarrollada por Damyz Solano y Zulma Solano.
---
+++ js/functions.js
@@ -0,0 +1,63 @@
+function sumar() {
+           
+    var dato1 = document.getElementById('dato1').value;
+    var dato2 = document.getElementById('dato2').value;
+    var resultado = document.getElementById('resultado');
+    resultado.value=parseFloat(dato1)+parseFloat(dato2);
+
+    var dato3 = prompt("Digite el primer número.");
+    var dato4 = prompt("Digite el segundo número");
+    var res = parseInt(dato3) + parseInt(dato4);
+    alert(res);
+    }
+  function restar() {
+    
+    var dato1 = document.getElementById('dato1').value;
+    var dato2 = document.getElementById('dato2').value;
+    var resultado = document.getElementById('resultado');
+    resultado.value=parseFloat(dato1)-parseFloat(dato2);
+
+    var dato3 = prompt("Digite el primer número.");
+    var dato4 = prompt("Digite el segundo número");
+    var res = parseInt(dato3) - parseInt(dato4);
+    alert(res);
+    }
+    function multiplicar() {
+   
+    var dato1 = document.getElementById('dato1').value;
+    var dato2 = document.getElementById('dato2').value;
+    var resultado = document.getElementById('resultado');
+    resultado.value=parseFloat(dato1)*parseFloat(dato2);
+
+    var dato3 = prompt("Digite el primer número.");
+    var dato4 = prompt("Digite el segundo número");
+    var res = parseInt(dato3) * parseInt(dato4);
+    alert(res);
+    }
+    function dividir() {
+    
+    var dato1 = document.getElementById('dato1').value;
+    var dato2 = document.getElementById('dato2').value;
+    var resultado = document.getElementById('resultado');
+    resultado.value=parseFloat(dato1)/parseFloat(dato2);
+    if (dato1==0 || dato2==0)
+    {
+        alert('Debes ingresar un número diferente de Cero');
+    }
+    else 
+    {
+     resultado.value=parseFloat(dato1)/parseFloat(dato2);
+    }
+
+    var dato3 = prompt("Digite el primer número.");
+    var dato4 = prompt("Digite el segundo número");
+    var res = parseInt(dato3) + parseInt(dato4);
+    if (dato3==0 || dato4==0)
+    {
+        alert('Debes ingresar un número diferente de Cero');
+    }
+    else 
+    {
+      alert(res);
+    }
+    }
