f0768f110e917fecc9d9e19f71d40d058b881480
Billy Everyteen <dzsolano@misena.edu.co>
2021-09-27 21:52:14 -0500

    first commit

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function sumar() {
+  function sumar() {
            
     var dato1 = document.getElementById('dato1').value;
     var dato2 = document.getElementById('dato2').value;
@@ -22,6 +22,7 @@ function sumar() {
     var res = parseInt(dato3) - parseInt(dato4);
     alert(res);
     }
+    
     function multiplicar() {
    
     var dato1 = document.getElementById('dato1').value;
@@ -34,30 +35,31 @@ function sumar() {
     var res = parseInt(dato3) * parseInt(dato4);
     alert(res);
     }
+
     function dividir() {
     
     var dato1 = document.getElementById('dato1').value;
     var dato2 = document.getElementById('dato2').value;
     var resultado = document.getElementById('resultado');
     resultado.value=parseFloat(dato1)/parseFloat(dato2);
-    if (dato1==0 || dato2==0)
-    {
-        alert('Debes ingresar un número diferente de Cero');
-    }
-    else 
-    {
-     resultado.value=parseFloat(dato1)/parseFloat(dato2);
-    }
+      if (dato1==0 || dato2==0)
+      {
+          alert('Debes ingresar un número diferente de Cero');
+      }
+      else 
+      {
+      resultado.value=parseFloat(dato1)/parseFloat(dato2);
+      }
 
-    var dato3 = prompt("Digite el primer número.");
-    var dato4 = prompt("Digite el segundo número");
-    var res = parseInt(dato3) + parseInt(dato4);
-    if (dato3==0 || dato4==0)
-    {
-        alert('Debes ingresar un número diferente de Cero');
-    }
-    else 
-    {
-      alert(res);
-    }
+      var dato3 = prompt("Digite el primer número.");
+      var dato4 = prompt("Digite el segundo número");
+      var res = parseInt(dato3) / parseInt(dato4);
+      if (dato3==0 || dato4==0)
+      {
+          alert('Debes ingresar un número diferente de Cero');
+      }
+      else 
+      {
+        alert(res);
+      }
     }
